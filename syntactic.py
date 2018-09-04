11c5186b35e781bee813f4a9d705759163ec49d1
Kevin Veloso <kevinveloso@gmail.com>
2018-09-04 06:24:00 -0300

    Verificação de variável completa.

--- syntactic.py
+++ syntactic.py
@@ -18,15 +18,25 @@ def isProgramId(line):
 
 def isVarDeclaration(line):
     isVarDec = False
-    variableTypes = ['integer', 'real', 'boolean']   
+    variableTypes = ['integer', 'real', 'boolean']  
+
     if(line[0][CLASS] == 'IDENTIFICADOR'):  
         if(line[1][TOKEN] == ':'):
             if(line[2][TOKEN] in variableTypes):                      
                 if(line[3][TOKEN] == ';'):
                     isVarDec = True
 
-    return isVarDec                
+        elif(line[1][TOKEN] == ','):
+            i = 2
+            while((line[i - 1][TOKEN] == ',') and (line[i][CLASS] == 'IDENTIFICADOR')):
+                if(line[i + 1][TOKEN] == ':'):
+                    if(line[i + 2][TOKEN] in variableTypes):                   
+                        if(line[i + 3][TOKEN] == ';'):
+                            isVarDec = True
+                            break
+                i += 2
 
+    return isVarDec     
 
 # def nextToken(self):
 #         if (self.index + 1) < len(self.list_tokens):
@@ -60,13 +70,13 @@ with open('./data/table.txt', 'r') as programTable:
             linesList.append(line) #ADICIONANDO NOVA PALAVRA NA LINHA
             lastLine = line[2]
         else:
-            programLines.append(linesList) #FAZENDO FECHAMENTO DA ÚLTIMA LINHA
+            programLines.append(linesList) #FAZENDO FECHAMENTO DA ULTIMA LINHA
             linesList = list() #ZERANDO LISTA
             linesList.append(line) #INICIANDO LISTA COM PRIMEIRA PALAVRA DA NOVA LINHA
             lastLine = line[2] #INCREMENTANDO O LASTLINE
-    programLines.append(linesList) #FAZENDO FECHAMENTO DA ÚLTIMA LINHA
+    programLines.append(linesList) #FAZENDO FECHAMENTO DA ULTIMA LINHA
 
-#   Compilador deve começar com program
+#   Compilador deve comecar com program
 #   depois deve vir o identificador e em seguida
 #   simbolo ; para indicar o fim do programa
 
@@ -81,7 +91,7 @@ with open('./data/table.txt', 'r') as programTable:
          
             while programLines: #CHECA PARA TODAS AS LINHAS SE VALEM COMO DEFINICAO DE TIPO DE VARIAVEL
                 if(isVarDeclaration(programLines[0])):
-                    programLines.remove(programLines[0])    
+                    programLines.remove(programLines[0])  
                 elif (programLines[0][TOKEN] == 'begin'):
                     break
                 else:
