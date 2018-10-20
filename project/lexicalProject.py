1e75ad153080fd5b79c53cd1a766f04a7b17e987
Fernando <fernandosouza@cc.ci.ufpb.br>
2018-10-20 19:11:58 -0300

    sintatico e inicio do semantico

--- project/lexicalProject.py
+++ project/lexicalProject.py
@@ -1,5 +1,6 @@
 # Alunos: Fernando Souza (11218354) e Kevin Veloso (11318626)
 import spacy
+import re
 import requests
 import sys
 from spacy.lang.pt.examples import sentences
@@ -9,15 +10,27 @@ from spacy.lang.pt.examples import sentences
 nlp = spacy.load('pt_core_news_sm')
 projectFile = open('./data/texto.txt', 'r')
 projectTable = open('./data/tabela.txt', 'w')
-
+# oficialTable = list()
 _file = nlp(projectFile.read())
 
 print(_file.text)
+r2 = requests.post('https://languagetool.org/api/v2/check', data={'text': _file.text, 'language': 'pt-br', 'enabledOnly': 'false'})
+resp = r2.json()
+concord = resp['matches'][0]['message']
+
+isConcord = re.split(r'em Português do Brasil utiliza-se', concord)
+
+
+if len(isConcord) == 1:
+    sys.exit(concord)
 
 for token in _file:
     r = requests.get('http://www.dicionarioweb.com.br/' + token.text)
     if r.status_code == 404:
         # print('palavra '+ token.text + 'esta escrita incorretamente')
-        sys.exit('palavra '+ token.text + 'esta escrita incorretamente')
-
+        sys.exit('palavra '+ token.text + ' esta escrita incorretamente')
+    # oficialTable.append(token.text + ' ' + token.pos_ + ' ' + token.dep_)
     projectTable.write(token.text + ' ' + token.pos_ + ' ' + token.dep_ + '\n')
+
+
+# print(oficialTable)
--- project/syntacticProject.py
+++ project/syntacticProject.py
@@ -25,31 +25,49 @@ def textoPoject_(officialList):
     textoPoject(officialList)
 
 def sentenca(officialList):
+    while officialList[0][TOKEN] is not '.':
+        
+        sintagma_nominal(officialList)
+
+        sintagma_verbal(officialList)
+        
+        # if officialList[0][CLASS] == 'NOUN':
+        #         officialList.remove(officialList[0])
+
+        # elif officialList[0][CLASS] == 'DET':
+        #     officialList.remove(officialList[0])
+        #     if officialList[0][CLASS] == 'NOUN':
+        #         officialList.remove(officialList[0])
+        #         if officialList[0][CLASS] != 'VERB' and officialList[0][TOKEN] != '.':
+        #             officialList.remove(officialList[0])
+
+        # elif officialList[0][CLASS] == 'VERB':
+        #     sintagma_verbal(officialList)
+        
+def sintagma_nominal(officialList):
     if officialList[0][CLASS] == 'NOUN':
-            officialList.remove(official[0])
-            # sintagma_nominal(officialList)
+        officialList.remove(officialList[0])
 
     elif officialList[0][CLASS] == 'DET':
         officialList.remove(officialList[0])
         if officialList[0][CLASS] == 'NOUN':
             officialList.remove(officialList[0])
-            # sintagma_nominal(officialList)
+            if officialList[0][CLASS] != 'VERB' and officialList[0][TOKEN] != '.':
+                officialList.remove(officialList[0])
 
-    
-    else:
-        sintagma_verbal(officialList)
+
 
 def sintagma_verbal(officialList):
     if officialList[0][CLASS] == 'VERB':
         officialList.remove(officialList[0])
-
-    elif officialList[0][CLASS] == 'VERB':
-        officialList.remove(officialList[0])
         if officialList[0][CLASS] == 'ADV':
             officialList.remove(officialList[0])
             sintagma_verbal(officialList)
+    else:
+        sentenca(officialList)
 
-    if len(officialList) > 0 :
+
+    if len(officialList) > 0 and officialList[0][TOKEN] != '.' :
         sintagma_verbal(officialList)
         
 
